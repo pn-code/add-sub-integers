1f4f50cbab4d44a5c615f5d16b6ebdc2af7f200b
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-13 23:13:15 -0700

    Added randomizer functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
 import Block from "@/components/Block";
 import BlockOrigin from "@/components/BlockOrigin";
 
 export default function Home() {
+    const [firstInteger, setFirstInteger] = useState<number | null>(null);
+    const [secondInteger, setSecondInteger] = useState<number | null>(null);
+    const [operator, setOperator] = useState<string | null>(null);
+
+    useEffect(() => {
+        randomizeEquation();
+    }, []);
+
+    const generateRandomNumberUpToTen = () => {
+        return Math.floor(Math.random() * 10) + 1;
+    };
+
+    const randomizeIntegers = () => {
+        setFirstInteger(generateRandomNumberUpToTen());
+        setSecondInteger(generateRandomNumberUpToTen());
+    };
+
+    const randomizeOperator = () => {
+        const randomNumber = Math.floor(Math.random() * 10) + 1;
+        setOperator(randomNumber > 5 ? "+" : "-");
+    };
+
+    const randomizeEquation = () => {
+        randomizeOperator();
+        randomizeIntegers();
+    };
+
     return (
         <main className="bg-black text-white w-full min-h-screen flex flex-col sm:flex-row gap-8 items-center sm:justify-center">
             {/* Sidebar */}
@@ -14,16 +43,15 @@ export default function Home() {
             {/* Main App */}
             <section className="flex flex-col gap-4 items-center">
                 {/* Equation Display */}
-                <section className="text-5xl mb-8">3 - 1</section>
+                <section className="text-5xl mb-8 flex gap-2 h-12">
+                    <span>{firstInteger}</span>
+                    <span>{operator}</span>
+                    <span>{secondInteger}</span>
+                </section>
 
                 {/* Positive Blocks */}
                 <section className="flex flex-col gap-2 items-center">
-                    <header className="flex gap-2 items-center">
-                        <h2 className="text-lg font-bold">Positive Blocks</h2>
-                        <button className="rounded-full bg-green-600 flex items-center justify-center px-2 py-1 text-sm">
-                            Add block
-                        </button>
-                    </header>
+                    <h2 className="text-lg font-bold">Positive Blocks</h2>
                     <section className="flex gap-2 border-y-2 items-center border-gray-200 w-full py-2 justify-center">
                         <Block color={"green"} />
                         <Block color={"green"} />
@@ -33,9 +61,8 @@ export default function Home() {
 
                 {/* Negative Blocks */}
                 <section className="flex flex-col gap-2 items-center">
-                    <header className="flex gap-2 items-center">
-                        <h2 className="text-lg font-bold">Negative Blocks</h2>
-                    </header>
+                    <h2 className="text-lg font-bold">Negative Blocks</h2>
+
                     <section className="flex gap-2 border-y-2 items-center border-gray-200 w-full py-2 justify-center">
                         <Block color={"red"} />
                     </section>
