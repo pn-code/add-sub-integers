0e276cc0864782395ec4e11e248a0ea9e5f4bf11
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-14 18:00:56 -0700

    Allow users to cross out blocks

--- components/Block.tsx
+++ components/Block.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Block({ positive }: { positive: boolean }) {
+    const [crossedOut, setCrossedOut] = useState(false);
+
     const blockStyles = positive
-        ? "w-12 h-12 bg-blue-500 border-white border-2 rounded-sm"
-        : "w-12 h-12 bg-red-500 border-white border-2 rounded-sm";
-    return <div draggable className={blockStyles}></div>;
+        ? "hover:bg-indigo-600 cursor-pointer w-12 h-12 bg-blue-500 border-white border-2 rounded-sm text-5xl flex items-center justify-center"
+        : "hover:bg-indigo-600 cursor-pointer w-12 h-12 bg-red-500 border-white border-2 rounded-sm text-5xl flex items-center justify-center";
+    return (
+        <div
+            onClick={() => setCrossedOut((prev) => !prev)}
+            className={blockStyles}
+        >
+            {crossedOut ? "X" : ""}
+        </div>
+    );
 }
