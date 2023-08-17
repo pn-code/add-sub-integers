44e69dd891eac1e80397eb8c09cf1425e0012a6d
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-16 21:08:23 -0700

    Added handleClick and isCrossed props

--- components/Block.tsx
+++ components/Block.tsx
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
-
-export default function Block({ positive }: { positive: boolean }) {
-    const [crossedOut, setCrossedOut] = useState(false);
-
+export default function Block({
+    positive,
+    handleClick,
+    isCrossed,
+}: {
+    positive: boolean;
+    handleClick: any;
+    isCrossed: boolean;
+}) {
     const blockStyles = positive
         ? "hover:bg-indigo-600 cursor-pointer w-12 h-12 bg-blue-500 border-white border-2 rounded-sm text-5xl flex items-center justify-center"
         : "hover:bg-indigo-600 cursor-pointer w-12 h-12 bg-red-500 border-white border-2 rounded-sm text-5xl flex items-center justify-center";
     return (
-        <div
-            onClick={() => setCrossedOut((prev) => !prev)}
-            className={blockStyles}
-        >
-            {crossedOut ? "X" : ""}
+        <div onClick={handleClick} className={blockStyles}>
+            {isCrossed ? "X" : ""}
         </div>
     );
 }
